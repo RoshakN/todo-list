f79e5cd6c6a14bd94ee9ce38238a8a2867d2ffba
RoshakN <msnooraniz@gmail.com>
2023-06-23 19:38:38 +0330

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Roshak&apos;s To Do List</h1>
+      <p>made with REACT</p>
+    </>
+  );
+}
+
+export default App;

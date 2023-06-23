ea40754a9d34c3f14bf706354b4d3f8cd6966469
RoshakN <msnooraniz@gmail.com>
2023-06-23 23:52:01 +0330

    FIX file extension

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-import React from "react";
-// import List from "./List";
+// import { useState } from "react";
+import List from "./List";
 import "./App.css";
 
 function App() {
-  const [counter, setCounter] = React.useState[0];
-
   return (
-    <>
+    <div>
       <h1>Roshak&apos;s To Do List</h1>
-      <p>made with REACT</p>
-      <button onClick={() => setCounter((count) => count + 1)}>
-        count is {counter}
-      </button>
-    </>
+      <h4>made by REACT</h4>
+      <List />
+    </div>
   );
 }
 
---
+++ src/List.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function List() {
+  const [count, setCount] = React.useState(0);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+}

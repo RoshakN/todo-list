92d73d3c25ac80b0edff62fbd03ac99e051611fe
RoshakN <msnooraniz@gmail.com>
2023-06-30 22:03:41 +0330

    Rewrite from scratch

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,60 @@
-// import { useState } from "react";
-import List from "./List";
+import { useState } from "react";
+// import List from "./List";
 import "./App.css";
 
 function App() {
+  const [toDoList, setToDoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const addTask = () => {
+    const newTaskObject = {
+      title: newTask,
+      id: toDoList.length === 0 ? 1 : toDoList[toDoList.length - 1].id + 1,
+    };
+    setToDoList([...toDoList, newTaskObject]);
+    clear();
+  };
+
+  const clear = () => {
+    setNewTask("");
+  };
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const handleDelete = (taskName) => {
+    setToDoList(toDoList.filter((task) => task.id !== taskName.id));
+  };
+
+  const handleEnter = (event) => {
+    if (event.key === "Enter") {
+      addTask();
+    }
+  };
+
   return (
-    <div>
+    <div className="App">
       <h1>Roshak&apos;s To Do List</h1>
-      <h4>made by REACT</h4>
-      <List />
+      <div className="new-task">
+        <input
+          type="text"
+          value={newTask}
+          onChange={handleChange}
+          onKeyDown={handleEnter}
+        />
+        <button onClick={addTask}>Add New Task</button>
+      </div>
+      <div className="list">
+        {toDoList.map((task) => {
+          return (
+            <div>
+              <p key={task.id}>{task.title}</p>
+              <button onClick={() => handleDelete(task)}>Delete Task</button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }

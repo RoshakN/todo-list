520c4e16c499f5d02e4f94a2404404ee6858ef23
RoshakN <msnooraniz@gmail.com>
2023-07-03 13:46:54 +0330

    Add list css

--- src/List.jsx
+++ src/List.jsx
@@ -1,15 +1,20 @@
 import PropTypes from "prop-types";
+import DeleteBin2FillIcon from "remixicon-react/DeleteBin2FillIcon";
+import CheckboxCircleFill from "remixicon-react/CheckboxCircleFillIcon";
 
 export default function List(props) {
   return (
     <div className="list-component">
       <p key={props.key}>{props.title}</p>
-      <button onClick={() => props.handleDelete(props.id)}>Delete Task</button>
+      <button onClick={() => props.handleDelete(props.id)}>
+        <DeleteBin2FillIcon className="trash-can" />
+      </button>
+
       <button
         onClick={() => props.handleComplete(props.id)}
-        style={{ backgroundColor: props.isCompleted ? "green" : "" }}
+        style={{ color: props.isCompleted ? "green" : "" }}
       >
-        Completed
+        <CheckboxCircleFill className="check" />
       </button>
     </div>
   );

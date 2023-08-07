66fba1ee2279270057b1bbb59b99c76326d1620f
RoshakN <msnooraniz@gmail.com>
2023-08-07 21:37:22 +0330

    feat: Add framer-motion

--- src/Tasks.jsx
+++ src/Tasks.jsx
@@ -2,24 +2,46 @@ import PropTypes from "prop-types";
 import DeleteBin2Fill from "remixicon-react/DeleteBin2FillIcon";
 import CheckboxBlankFill from "remixicon-react/CheckboxBlankFillIcon";
 import CheckboxFill from "remixicon-react/CheckboxFillIcon";
+import { motion } from "framer-motion";
 
 export default function Tasks(props) {
+  const handle = {
+    handleScale: {
+      scale: 1.3,
+    },
+  };
   return (
     <div className="list-component">
-      <p key={props.key} className="task-title">
+      <motion.p
+        key={props.key}
+        className="task-title"
+        animate={{
+          x: "2rem",
+          transition: {
+            stiffness: 20,
+          },
+        }}
+        variants={handle}
+        whileHover="handleScale"
+      >
         {props.title}
-      </p>
+      </motion.p>
       <button onClick={() => props.handleDelete(props.id)} title="Delete">
         <DeleteBin2Fill className="trash-can" />
       </button>
 
-      <button onClick={() => props.handleComplete(props.id)} title="Done">
+      <motion.button
+        onClick={() => props.handleComplete(props.id)}
+        title="Done"
+        variants={handle}
+        whileTap="handleScale"
+      >
         {props.isCompleted === true ? (
           <CheckboxFill className="check complete" />
         ) : (
           <CheckboxBlankFill className="check" />
         )}
-      </button>
+      </motion.button>
     </div>
   );
 }
